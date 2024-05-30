01577b151467aa578af3ceba54c3621fa9923ca9
Sam199416 <psambhavi855@gmail.com>
2024-05-30 13:46:32 -0500

    Department back end First commit

---
+++ src/main/java/com/microservices/department/service/DepartmentService.java
@@ -0,0 +1,27 @@
+package com.microservices.department.service;
+
+import com.microservices.department.entity.Department;
+import com.microservices.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Service
+@Slf4j
+public class DepartmentService {
+    @Autowired
+    private DepartmentRepository departmentRepository;
+    public Department saveDepartment( Department department) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
+}
+
